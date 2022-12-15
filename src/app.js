d21a78e0591b2a38ffd0a34712ed976f072bedd7
Nico <nicoabdala722@icloud.com>
2022-12-15 00:09:36 -0300

    creando repo

---
+++ src/app.js
@@ -0,0 +1,19 @@
+// Requerimos express y lo ejecutamos para tener disponibles todos los metodos que vamos a precisar
+const express = require('express');
+const app = express();
+// Modulo nativo para manejar las rutas de los archivos
+const path = require('path');
+const port = process.env.PORT || 3000;
+
+// Usando recursos estáticos.
+app.use(express.static( './public' ));
+
+// Ponemos a escuchar el servidor
+app.listen(port, () => console.log('Server running on http://localhost:' + port));
+
+// Definimos las rutas a los distintos pedidos que nuestro sitio sabe responder
+app.get('/', (req, res) => res.sendFile( path.resolve(__dirname, './views/index.html') ));
+
+app.get('/register', (req, res) => res.sendFile( path.resolve(__dirname, './views/register.html') ));
+
+app.get('/login', (req, res) => res.sendFile( path.resolve(__dirname, './views/login.html') ));
